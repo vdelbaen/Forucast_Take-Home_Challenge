4dc44e7d9bd65f6080e61234752afc6b8b8fb222
Delbaen <vincent.delbaen@accenture.com>
2020-11-22 16:25:40 +0100

    init

---
+++ cloud_function/bq_load.py
@@ -0,0 +1,40 @@
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+table_id = "airy-berm-296308.Fourcast_Take_Home_Challenge.taxi_location_data_batch"
+
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("unique_key", "STRING"),
+        bigquery.SchemaField("taxi_id", "STRING"),
+        bigquery.SchemaField("trip_start_timestamp", "TIMESTAMP"),
+        bigquery.SchemaField("trip_end_timestamp", "TIMESTAMP"),
+        bigquery.SchemaField("pickup_latitude", "FLOAT"),
+        bigquery.SchemaField("pickup_longitude", "FLOAT"),
+        bigquery.SchemaField("dropoff_latitude", "FLOAT"),
+        bigquery.SchemaField("dropoff_longitude", "FLOAT"),
+        bigquery.SchemaField("pickup_latlong", "STRING"),
+        bigquery.SchemaField("dropoff_latlong", "STRING"),
+        bigquery.SchemaField("pickup_community_area", "INTEGER"),
+        bigquery.SchemaField("dropoff_community_area", "INTEGER"),
+
+    ],
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+
+def process_interview(data, context):
+    print('new csv file uploaded...')
+    uri = "gs://" + data['bucket'] + "/" + data['name']
+    load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)
+    load_job.result()  # Waits for the job to complete.
+    destination_table = client.get_table(table_id)
+    print("Loaded {} rows.".format(destination_table.num_rows))
+
+
+
+
+
---
+++ gcloudSDK_init_script.ps1
@@ -0,0 +1,4 @@
+gcloud config set project airy-berm-296308
+
+
+gsutil mb -p airy-berm-296308 -c Standard -l europe-west1 -b on gs://taxi_csv
